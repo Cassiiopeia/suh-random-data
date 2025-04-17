9853ec0536ceb4ce7cd09434b48e2920a0a686cf
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-04-17 21:18:06 +0900

    프로젝트 기본 골격 추가 : feat : 프로젝트 시작 https://github.com/Cassiiopeia/suh-nickname-generator/issues/1

---
+++ src/main/java/me/suhsaechan/suhnicknamegenerator/SuhNicknameGeneratorApplication.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhnicknamegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhNicknameGeneratorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SuhNicknameGeneratorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/suhsaechan/suhnicknamegenerator/SuhNicknameGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhnicknamegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhNicknameGeneratorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
