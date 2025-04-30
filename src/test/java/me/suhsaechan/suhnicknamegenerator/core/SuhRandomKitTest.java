5d556de58dccc0ca525deee49abf359bb7b39c9e
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-04-30 18:33:24 +0900

    정치인 랜덤 닉네임 추가 : feat : 정치인 텍스트 추가 및 DOCS 업데이트 https://github.com/Cassiiopeia/suh-random-engine/issues/11

--- src/test/java/me/suhsaechan/suhnicknamegenerator/core/SuhRandomKitTest.java
+++ src/test/java/me/suhsaechan/suhnicknamegenerator/core/SuhRandomKitTest.java
@@ -9,7 +9,8 @@ class SuhRandomKitTest {
   @Test
   public void mainTest(){
 //    nickName_테스트();
-    matureNickName_테스트();
+//    matureNickName_테스트();
+    politicianNickName_테스트();
   }
 
   private void nickName_테스트() {
@@ -31,12 +32,29 @@ class SuhRandomKitTest {
         .locale("ko")        // 한/영/코드 모두 OK
         .numberLength(4)                // 4자리 숫자
         .uuidLength(4)                  // 4자리 UUID
+        .enableAdultContent(true)
         .build();
 
     log.info("========================");
     log.info("matureNickname:       {}", suhRandomKit.matureNickname());
     log.info("matureNicknameWithNumber:   {}", suhRandomKit.matureNicknameWithNumber());
     log.info("matureNicknameWithUuid:     {}", suhRandomKit.matureNicknameWithUuid());
+    log.info("========================");
+
+  }
+
+  private void politicianNickName_테스트(){
+    SuhRandomKit suhRandomKit = SuhRandomKit.builder()
+        .locale("ko")        // 한/영/코드 모두 OK
+        .numberLength(4)                // 4자리 숫자
+        .uuidLength(4)                  // 4자리 UUID
+        .enableAdultContent(true)
+        .build();
+
+    log.info("========================");
+    log.info("politicianNickname:       {}", suhRandomKit.politicianNickname());
+    log.info("politicianNicknameWithNumber:   {}", suhRandomKit.politicianNicknameWithNumber());
+    log.info("politicianNicknameWithUuid:     {}", suhRandomKit.politicianNicknameWithUuid());
     log.info("========================");
 
   }
